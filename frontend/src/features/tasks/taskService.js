8b0d78d9f8637bab09ba9df8a4265b60b4b4c8aa
codesarray <codesarray96@gmail.com>
2025-06-13 00:43:06 +0530

    frontend

--- frontend/src/features/tasks/taskService.js
+++ frontend/src/features/tasks/taskService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = "/api/tasks/";
+const API_URL = `${process.env.API_URL}/api/tasks/`;
 
 const getTasks = async (filters) => {
   const response = await axios.get(API_URL, { params: filters });
